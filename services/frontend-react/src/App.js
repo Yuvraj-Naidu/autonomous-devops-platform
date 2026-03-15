403b11bac42db7c757ff5f6f5d3e1cc61742c60c
Yuvraj Naidu <yuvrajkumar0066@gmail.com>
2026-03-15 22:12:07 +0530

    feat(frontend): add React dashboard service

---
+++ services/frontend-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Autonomous DevOps Platform</h1>
+      <p>Frontend Dashboard Running</p>
+    </div>
+  );
+}
+
+export default App;
