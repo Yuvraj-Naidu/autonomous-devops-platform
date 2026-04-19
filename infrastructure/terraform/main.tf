eeac61c90ca454b16c8cb62b3e4043e9c3213ce1
Yuvraj Naidu <yuvrajkumar0066@gmail.com>
2026-04-20 00:27:07 +0530

    feat(terraform): automate EC2 and security group provisioning with variables support

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,16 +1,46 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
 resource "aws_instance" "devops_server" {
   ami           = "ami-07216ac99dc46a187" # updated Ubuntu
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   key_name = "devops-key" # AWS key
 
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
   tags = {
     Name        = "autonomous-devops-ec2"
-    Project     = "autonomous-devops-platform" # project name 
-    Environment = "dev"
+    Project     = var.project_name # project name 
+    Environment = var.environment
+  }
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Allow web and SSH access"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {}
+
+variable "project_name" {
+  default = "autonomous-devops-platform"
+}
+
+variable "environment" {
+  default = "dev"
+}
